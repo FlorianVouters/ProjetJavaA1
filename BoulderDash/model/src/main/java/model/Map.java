429659d25ae9c65263350135cb6f5d4ca4f6aa26
NHURTEVENT <nicolas.hurtevent@viascesi.fr>
2017-06-15 16:06:14 +0200

    partial update of Map (and test of my Git account)

--- BoulderDash/model/src/main/java/model/Map.java
+++ BoulderDash/model/src/main/java/model/Map.java
@@ -1,5 +1,39 @@
 package model;
 
-public class Map {
+import java.util.Observable;
 
+public class Map extends Observable implements IMap {
+
+	private int height; //height of the map
+	private int width; // width of the map
+	private IElement[][] map; //double array of element which constitute the map
+	
+	public Map(int level){
+		loadLevel(level);
+	}
+	
+	private void loadLevel(int level) {
+		// TODO Auto-generated method stub
+		//goes get the map in the DB
+	}
+
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public IElement[][] getMap() {
+		return map;
+	}
+	public void setMap(IElement[][] map) {
+		this.map = map;
+	}
+	
 }
