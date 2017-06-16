e3829226e3c9a40066f6532c1008451009b20a27
NHURTEVENT <nicolas.hurtevent@viacesi.fr>
2017-06-16 16:36:04 +0200

    Appendment of a forgoten specific element comportement needing its own class

---
+++ BoulderDash/model/src/main/java/model/element/motionfull/Amoeba.java
@@ -0,0 +1,18 @@
+package model.element.motionfull;
+
+public class Amoeba extends Enemy{
+
+	int timer; //multiply at some point and turns into a rock at the end
+	
+	public void kill(){
+		//turn any enemy into 9 diamonds
+	}
+	
+	public void die(){
+		//if surrounded by four rocks turns into diamond
+	}
+	
+	public void multiply(){
+		//multiplies at random moments
+	}
+}
