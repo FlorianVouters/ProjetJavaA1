9253e4354a4bf389aeb083b4067f01e8bf2852ce
NHURTEVENT <nicolas.hurtevent@viacesi.fr>
2017-06-21 14:16:47 +0200

    didn't I just push them ?

--- BoulderDash/model/src/main/java/model/Map.java
+++ BoulderDash/model/src/main/java/model/Map.java
@@ -10,13 +10,8 @@ import java.util.List;
 import java.util.Observable;
 
 import model.dao.ElementDAO2;
-<<<<<<< HEAD
 import model.element.ElementFactory;
-=======
-import model.element.motionfull.Direction;
->>>>>>> branch 'model' of https://github.com/FlorianVouters/ProjetJavaA1
 import model.element.motionfull.MotionfullElement;
-import model.element.motionless.Background;
 
 public class Map extends Observable implements IMap {
 
@@ -120,46 +115,6 @@ public class Map extends Observable implements IMap {
 	public Observable getObservable() {
 		return this;
 	}
-	
-	public boolean isEmpty(int x, int y) {
-		if (getElementByPosition(x, y).getClass().equals(Background.class)){
-			return true;
-		}
-		return false;
-	}
-	public lookForAndMoveEnemy() {
-		IElement[][] elem = getMap();
-    		for (int j = 0; j <elem.length; j++) {
-    			for (int i = 0; i < elem[i].length; i++) {
-    				if (getMap().getElementByPosition(i, j).getClass().equals(Enemy.class) {
-    					moveEnemy(getMap().getElementByPosition(i, j));
-    				}
-    			}
-    		}
-				    }
-	
-	public void moveEnemy(MotionfullElement element) {
-		int x = 0; int y = 0;
-		x = element.getX();
-		y = element.getY();
-		
-		if (isEmpty(x+1, y)) {
-			element.setX(x+1);
-			element.setY(y);
-		}else if (isEmpty(x, y-1) && !isEmpty(x+1, y)) {
-			element.setX(x);
-			element.setY(y-1);
-		}else if (isEmpty(x-1, y) && !isEmpty(x, y-1) && !isEmpty(x+1, y)) {
-			element.setX(x-1);
-			element.setY(y);
-		}else if (isEmpty(x, y-1) && !isEmpty(x-1, y) && !isEmpty(x, y-1) && !isEmpty(x+1, y)) {
-			element.setX(x);
-			element.setY(y-1);
-		}else {
-			element.doNothing();
-		}
-	}
-	
 	
 	
 	@Deprecated //only used to push the maps in the database
