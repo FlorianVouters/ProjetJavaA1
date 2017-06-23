249311e9f31ba7cf0972c20529c3e59eccce549d
FlorianVouters <florian.vouters@viacesi.fr>
2017-06-23 11:40:15 +0200

    New sprites on MainCharacter according to movements

--- BoulderDash/model/src/main/java/model/element/motionfull/MainCharacter.java
+++ BoulderDash/model/src/main/java/model/element/motionfull/MainCharacter.java
@@ -2,6 +2,10 @@ package model.element.motionfull;
 
 import java.awt.Image;
 import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
 
 import model.IDestructible;
 import model.Map;
@@ -11,6 +15,16 @@ import model.Sprite;
 public class MainCharacter extends MotionfullElement implements IDestructible{
 
 	private static final Sprite MAIN_CHARACTER = new Sprite('H');
+	
+	BufferedImage buffer;
+	
+	private static final BufferedImage MAIN_CHARACTER_LEFT = ImageIO.read(new File("pics/charspritesheet.png")).getSubimage(0, 16, 16, 16);
+	
+	private static final BufferedImage MAIN_CHARACTER_RIGHT = ImageIO.read(new File("pics/charspritesheet.png")).getSubimage(0, 48, 16, 16);
+	
+	private static final BufferedImage MAIN_CHARACHTER_DOWN = ImageIO.read(new File("pics/charspritesheet.png")).getSubimage(0, 64, 16, 16);
+	
+	private static final BufferedImage MAIN_CHARACTER_UP = ImageIO.read(new File("pics/charspritesheet.png")).getSubimage(0, 32, 16, 16);
 	
 	
 	public MainCharacter(Map map){
